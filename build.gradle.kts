9d34594802956fc847e78de8c0db7bb14b992d80
Paul Harrison <paul.harrison@manchester.ac.uk>
2024-01-08 13:14:56 +0000

    update to use latest plugin
    also properly add the gradle wrapper, and update some of the other dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     // this plugin provides all the vo-dml functionality
-    id("net.ivoa.vo-dml.vodmltools") version "0.3.23"
+    id("net.ivoa.vo-dml.vodmltools") version "0.4.2"
 }
 
 vodml {
@@ -26,13 +26,13 @@ dependencies {
     api("org.javastro.ivoa.vo-dml:ivoa-base:1.0-SNAPSHOT") // IMPL using API so that it appears in transitive compile
 
     // the dependencies below are related to testing
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
 
     implementation("org.slf4j:slf4j-api:1.7.32")
-    testRuntimeOnly("ch.qos.logback:logback-classic:1.2.3")
+    testRuntimeOnly("ch.qos.logback:logback-classic:1.4.12")
 
-    testImplementation("org.apache.derby:derby:10.14.2.0")
+    testImplementation("org.apache.derby:derby:10.15.2.1")
     testImplementation("org.javastro:jaxbjpa-utils:0.1.2")
     testImplementation("org.javastro:jaxbjpa-utils:0.1.2:test")
 }
