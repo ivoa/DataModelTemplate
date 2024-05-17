d9eaa7189f36fa2c39644f7f10b6073fd0f6c2b7
Paul Harrison <paul.harrison@manchester.ac.uk>
2024-05-17 08:12:41 +0100

    update to 0.5.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     // this plugin provides all the vo-dml functionality
-    id("net.ivoa.vo-dml.vodmltools") version "0.5.0"
+    id("net.ivoa.vo-dml.vodmltools") version "0.5.1"
 }
 
 vodml {
