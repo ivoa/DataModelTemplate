d1de44e8c821de8298887e15520ffa2815d9e619
Paul Harrison <paul.harrison@manchester.ac.uk>
2025-09-02 14:59:03 +0100

    update logback to avoid CVE

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,7 +36,7 @@ dependencies {
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
 
     implementation("org.slf4j:slf4j-api:1.7.32")
-    testRuntimeOnly("ch.qos.logback:logback-classic:1.4.12")
+    testRuntimeOnly("ch.qos.logback:logback-classic:1.5.13")
 
     testImplementation("com.h2database:h2:2.2.220")
     testImplementation("org.javastro:jaxbjpa-utils:0.2.3")
